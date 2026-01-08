a63ed2600178241834c611e2588be4bcb27e171a
grumpycatyo-collab <linkolnmax24@gmail.com>
2026-01-08 20:35:15 +0200

    feat: add primary pika init

---
+++ pika/cli.py
@@ -0,0 +1,33 @@
+import argparse
+from pathlib import Path
+
+from . import data
+
+def main():
+    args = parse_args()
+    args.func(args)
+
+
+def parse_args ():
+    parser = argparse.ArgumentParser()
+
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(func=init)
+
+    hash_object_parser = commands.add_parser('hash-object')
+    hash_object_parser.set_defaults(func=hash_object)
+    hash_object_parser.add_argument('file')
+
+    return parser.parse_args()
+
+
+def init(args):
+    data.init()
+    print(f'Initialized empty pika repo in {Path.cwd() / data.GIT_DIR}')
+
+def hash_object(args):
+    with open(args.file, 'rb') as f:
+        print(data.hash_object(f.read()))
---
+++ pika/data.py
@@ -0,0 +1,17 @@
+import os
+import hashlib
+from pathlib import Path
+
+GIT_DIR = Path(".pika")
+OBJECTS_DIR = GIT_DIR / "objects"
+
+def init():
+    os.makedirs(GIT_DIR)
+    os.makedirs(OBJECTS_DIR)
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(OBJECTS_DIR / oid, 'wb') as out:
+        out.write(data)
+    return oid
