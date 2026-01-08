57244f328ac6fe4bfde8dcd5dd1617975eb9e853
grumpycatyo-collab <linkolnmax24@gmail.com>
2026-01-08 21:39:46 +0200

    feat: create the actual tree with pika

--- pika/base.py
+++ pika/base.py
@@ -4,13 +4,23 @@ from . import data
 
 
 def write_tree(directory='.'):
+    entries = []
     with os.scandir(directory) as it:
         for entry in it:
             full = Path(directory) / entry.name
+            if is_ignored(full):
+                continue
             if entry.is_file(follow_symlinks=False):
-                # TODO write the file to object store
-                print(full)
+                type_ = 'blob'
+                with open(full, 'rb') as f:
+                    oid = data.hash_object(f.read())
+                entries.append((entry.name, oid, type_))
             elif entry.is_dir(follow_symlinks=False):
-                write_tree(full)
+                type_ = 'tree'
+                oid = write_tree(full)
+                entries.append((entry.name, oid, type_))
+        tree = ''.join(f'{type_} {oid} {name}\n' for name, oid, type_ in sorted (entries))
+        return data.hash_object(tree.encode(), 'tree')
 
-    # TODO actually create the tree object
+def is_ignored(path: Path):
+    return '.pika' in path.parts
